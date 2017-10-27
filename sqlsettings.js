787be0c60aeefd8b5b12ecbab6235cec566dfba2
Jay Steingold <jay.steingold@gmail.com>
2017-10-27 13:42:20 -0700

    added port

--- sqlsettings.js
+++ sqlsettings.js
@@ -6,7 +6,7 @@ module.exports = {
   user: 'b8be93e3c1fe2a',
   password: '1cc9aca0',
   database: 'heroku_c246010517e44f7',
-
+  port: 3306,
 };
 
 // mysql://b8be93e3c1fe2a:1cc9aca0@us-cdbr-iron-east-05.cleardb.net/heroku_c246010517e44f7?reconnect=true
