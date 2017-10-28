5391677873c7504285e954a3863c7234ed1c6974
Jay Steingold <jay.steingold@gmail.com>
2017-10-28 14:14:03 -0700

    change

--- app.js
+++ app.js
@@ -20,14 +20,14 @@ var award = require('./routes/award');
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 //Referenced: https://vladimirponomarev.com/blog/authentication-in-react-apps-jwt
 //Referenced: https://github.com/XBLDev/ReactJSNodejsAuthRouterv4
